8be6967fd69aef9cb650f69460cd7d901dd25877
ZitounBedis <bediszitoun1919@gmail.com>
2021-01-07 13:32:28 +0100

    repo

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <landing />
+    
+  </div>
+</template>
+
+<script>
+import landing from "./components/landing.vue";
+
+export default {
+  name: "App",
+  components: {
+    landing,
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ client/src/components/landing.vue
@@ -0,0 +1,30 @@
+
+<template>
+  <div class="hello">
+  <vsButton   line-position="top" line-origin="right" color="dark" type="line">Sign in</vsButton></div>
+</template>
+
+<script>
+import Vue from 'vue'
+import { vsButton } from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+Vue.use(vsButton)
+export default {
+  name: "landing",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+ body{
+  background-color : #f8b101
+} 
+.hello {
+  background-image: url("../background.png");
+  background-repeat: no-repeat;
+  background-size:  85% ;
+  height: 100vh;
+
+background-position: center;
+}
+</style>
